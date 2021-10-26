50f41b4fc3355ebdd0fcd55fca6fef4e6f003fcc
Glaas <s.decuyper314@gmail.com>
2021-10-26 09:31:56 +0200

    Basic enemy spawn

---
+++ TowerOffense/Assets/EnemySpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+   public GameObject enemyPrefab;
+   public Transform enemyParent;
+
+   private void Awake() {
+       enemyParent = GameObject.Find("--Enemies--").transform;
+       StartCoroutine(nameof(EnemySpawn));
+
+   }
+
+   IEnumerator EnemySpawn(){
+       var enemy = GameObject.Instantiate(enemyPrefab, transform.position, Quaternion.identity,enemyParent);
+       yield return new WaitForSeconds(2.5f);
+       StartCoroutine(nameof(EnemySpawn));
+   }
+}
