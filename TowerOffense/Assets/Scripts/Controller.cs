643b4f188cfb576166b7740f38b4ca139e58422d
Sebastien "Glaas" Decuyper <s.decuyper314@gmail.com>
2021-10-31 03:18:40 +0100

    helper function to display enemy path

--- TowerOffense/Assets/Scripts/Controller.cs
+++ TowerOffense/Assets/Scripts/Controller.cs
@@ -22,15 +22,29 @@ public class Controller : MonoBehaviour
     {
         ComputeDistanceToTower();
 
-//TODO implement attack behaviour
-      //  switch (state) 
-      //  {
-//
-      //      default:
-      // }
+
+        //TODO implement attack behaviour
+        //  switch (state) 
+        //  {
+        //
+        //      default:
+        // }
 
     }
     void ComputeDistanceToTower() => distanceToTower = Vector3.Distance(target.position, transform.position);
 
-
+    private void OnDrawGizmosSelected()
+    {
+        foreach (var item in agent.path.corners)
+        {
+            Gizmos.DrawWireSphere(item, .25f);
+        }
+        for (int i = 0; i < agent.path.corners.Length; i++)
+        {
+            if (i==0)continue;
+            if (i+1>=agent.path.corners.Length)continue;
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(agent.path.corners[i], agent.path.corners[i+1]);
+        }
+    }
 }
