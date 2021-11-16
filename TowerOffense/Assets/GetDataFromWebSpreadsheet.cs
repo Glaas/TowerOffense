88bf0c2c66128ff0c61c6d311c6d3a445c51bb6e
Sebastien "Glaas" Decuyper <s.decuyper314@gmail.com>
2021-11-16 01:36:49 +0100

    spreadsheet progress

--- TowerOffense/Assets/GetDataFromWebSpreadsheet.cs
+++ TowerOffense/Assets/GetDataFromWebSpreadsheet.cs
@@ -1,44 +1,86 @@
+using System;
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using SimpleJSON;
+using NaughtyAttributes;
+using System.IO;
 
 public class GetDataFromWebSpreadsheet : MonoBehaviour
 {
+    public string googleSheetsAPIURL = "https://sheets.googleapis.com/v4/spreadsheets";
+    public string sheetID = "1Zl2wV337Mjls1dJSckvE-ThFl8MGrbNDyDtYWj_aD0E";
+    public string APIKey = "AIzaSyAeBeTsGl9dUuq2S9Y9fO28qZ8ZJhL2i70";
+    public string worksheetName = "Sheet1";
+    public string startCell, endCell;
+
+
+
+    public enum Get_Type { SINGLE_CELL, RANGE_OF_CELLS };
+
+
     void Start()
     {
+
+        var builtString = new StringBuilder();
+        builtString.Append(googleSheetsAPIURL);
+        builtString.Append("/" + sheetID);
+        builtString.Append("/values");
+        builtString.Append("/" + worksheetName + "!" + startCell + ":" + endCell);
+        builtString.Append("?key=" + APIKey);
+
+
+
         // Initiate the web request
-        StartCoroutine(GetRequest("https://www.ethercalc.org/_/7xbtpaxxhc69/cells/A1"));        
+        StartCoroutine(GetRequest(builtString.ToString()));
     }
+
 
     // Taken from the Unity Documentation:
     // https://docs.unity3d.com/ScriptReference/Networking.UnityWebRequest.Get.html
     IEnumerator GetRequest(string uri)
     {
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+
+        UnityWebRequest webRequest = UnityWebRequest.Get(uri);
+        yield return webRequest.SendWebRequest();
+
+        string[] pages = uri.Split('/');
+        int page = pages.Length - 1;
+
+        if (webRequest.result == UnityWebRequest.Result.ConnectionError)
         {
-            // Request and wait for the desired page.
-            yield return webRequest.SendWebRequest();
-
-            string[] pages = uri.Split('/');
-            int page = pages.Length - 1;
-
-            if (webRequest.result == UnityWebRequest.Result.ConnectionError)
-            {
-                Debug.Log(pages[page] + ": Error: " + webRequest.error);
-            }
-            else
-            {
-                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
-
-                // Create a JSON object from received string data
-                JSONNode jsonNode = SimpleJSON.JSON.Parse(webRequest.downloadHandler.text);
-
-                // Get a single element from JSON data
-                Debug.Log ("datavalue of the cell is: "+ jsonNode["datavalue"].ToString());
-
-            }
+            Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            //Debug.Log(": Error: " + webRequest.error);
         }
+        else
+        {
+            Debug.Log("Received: " + webRequest.downloadHandler.text + "\nPage title : " + pages[page] + "\n\n\n");
+
+            //Create a JSON object from received string data
+            JSONNode jsonNode = SimpleJSON.JSON.Parse(webRequest.downloadHandler.text);
+
+            var myJ = JsonUtility.ToJson(webRequest.downloadHandler.text);
+
+            //print current date and time
+            var myDate = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            var path = "Assets/SavedFiles/";
+            File.WriteAllText(path + myDate + ".json", jsonNode.ToString());
+
+        }
+
+    }
+}
+[System.Serializable]
+public class MySpreadsheetData
+{
+    public string playerName;
+    public int lives;
+    public float health;
+
+    public string SaveToString()
+    {
+        return JsonUtility.ToJson(this);
     }
 }
