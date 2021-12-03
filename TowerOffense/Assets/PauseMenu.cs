1169a1b554db60d17d38fea8df4c2dd3d91932d9
Marie Wispler <marie.wispler@ue-germany.de>
2021-12-03 16:00:07 +0100

    got the slider to function visually

--- TowerOffense/Assets/PauseMenu.cs
+++ TowerOffense/Assets/PauseMenu.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
+    //public AudioMixer unityAudioMixer;
+
+    private GameObject SliderFunction;
+
+    private void Start()
+    {
+        GameObject.Find("SliderFunction");
+        SliderFunction.GetComponent<Slider>();
+    }
+    
     public void SetVolume(float volume)
     {
+        //unityAudioMixer.SetFloat("volume", volume);
         
+        //change audiolistener volume instead
+
+        AudioListener.volume.Equals(SliderFunction);
     }
 }
