0f6ae2c415b60e3ab769dd185a5355bc6fce073d
Marie Wispler <marie.wispler@ue-germany.de>
2021-12-04 16:32:32 +0100

    implemented more pause menu buttons
    - quit game button
    - restart scene button
    - open feedback form button

--- TowerOffense/Assets/PauseMenuButtons.cs
+++ TowerOffense/Assets/PauseMenuButtons.cs
@@ -1,29 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PauseMenuButtons : MonoBehaviour
 {
-
+    public GameObject feedbackCanvas;
 
     public void Start()
     {
-        
+        feedbackCanvas = GameObject.Find("FeedbackCanvas");
+        feedbackCanvas.SetActive(false);
     }
 
     public void RestartScene()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void Quit()
     {
-        
+        Application.Quit();
     }
 
     public void FeedbackButton()
     {
-        
+        feedbackCanvas.SetActive(true);
     }
 }
