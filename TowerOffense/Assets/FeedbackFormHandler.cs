94bf136b984488315343a06eedb6344403ae3429
Sebastien "Glaas" Decuyper <s.decuyper314@gmail.com>
2021-12-06 16:30:11 +0100

    Working feedback form

--- TowerOffense/Assets/FeedbackFormHandler.cs
+++ TowerOffense/Assets/FeedbackFormHandler.cs
@@ -1,9 +1,16 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
 
 public class FeedbackFormHandler : MonoBehaviour
 {
+    public ToggleGroup moodToggleGroup;
+    public ToggleGroup formTypeToggleGroup;
+    public TMP_InputField writtenFeedbackInputField;
     public GameObject feedbackForm;
+
     private void Start()
     {
         if (feedbackForm == null)
@@ -14,18 +21,30 @@ public class FeedbackFormHandler : MonoBehaviour
 
     }
 
-    void ToggleFeedbackForm()
+    public void ToggleFeedbackForm()
     {
-        feedbackForm.SetActive(!feedbackForm.activeSelf);
+        if (!feedbackForm.activeSelf)
+        {
+            feedbackForm.SetActive(true);
+            feedbackForm.transform.localScale = Vector3.zero;
+            feedbackForm.transform.DOScale(1, 0.5f).SetEase(Ease.OutBack);
+        }
+        else
+        {
+            feedbackForm.transform.DOScale(0, 0.5f).SetEase(Ease.InBack).OnComplete(() => feedbackForm.SetActive(false));
+
+        }
+       
     }
 
     public void SendFeedbackForm()
     {
         FeedbackForm feedbackForm = new FeedbackForm();
         feedbackForm._id = DateTime.Now.ToString(("dd_MM_yyyy_HH_mm_ss"));
-        feedbackForm.PlayerMood = "neutral";
-        feedbackForm.FeedbackType = "feedback";
-        feedbackForm.FeedbackText = "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+        feedbackForm.date = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+        feedbackForm.PlayerMood = ReturnMoodPicked();
+        feedbackForm.FeedbackType = ReturnFormTypePicked();
+        feedbackForm.FeedbackText = writtenFeedbackInputField.text;
         feedbackForm.WaveNumber = FindObjectOfType<WaveManager>().currentWave;
         feedbackForm.CurrentAmountOfMoney = GlobalDataHandler.instance.currentPlayerCoins;
         feedbackForm.EnemiesKilled = GlobalDataHandler.instance.enemiesKilled;
@@ -48,26 +67,63 @@ public class FeedbackFormHandler : MonoBehaviour
             SendFeedbackForm();
         }
     }
-}
-public enum PlayerMood
-{
-    Happy,
-    Neutral,
-    Sad
+    string ReturnMoodPicked()
+    {
+        switch (moodToggleGroup.GetFirstActiveToggle().name.ToLower())
+        {
+            case "happytoggle":
+                return "happy";
+            case "sadtoggle":
+                return "sad";
+            case "neutraltoggle":
+                return "neutral";
+            default:
+                throw new ArgumentException("No mood picked");
+        }
+    }
+    string ReturnFormTypePicked()
+    {
+        switch (formTypeToggleGroup.GetFirstActiveToggle().name.ToLower())
+        {
+            case "bugtoggle":
+                return "bug";
+            case "suggestiontoggle":
+                return "suggestion";
+            case "sayhitoggle":
+                return "I just want to say hi";
+            case "othertoggle":
+                return "other";
+            default:
+                throw new ArgumentException("No form type picked");
+        }
+    }
+    private void Awake()
+    {
+        if (moodToggleGroup == null)
+        {
+            moodToggleGroup = GameObject.Find("SmileysParent").GetComponent<ToggleGroup>();
+        }
+        if (formTypeToggleGroup == null)
+        {
+            formTypeToggleGroup = GameObject.Find("FormTypeToggleGroup").GetComponent<ToggleGroup>();
+        }
+        if (writtenFeedbackInputField == null)
+        {
+            writtenFeedbackInputField = GameObject.Find("WrittenFeedbackPanel").GetComponentInChildren<TMP_InputField>();
+        }
 
-}
-public enum FeedbackType
-{
-    Bug,
-    Suggestion,
-    Complaint,
-    Other
+
+
+
+
+    }
 }
 
 [Serializable]
 public class FeedbackForm
 {
     public string _id;
+    public string date;
     public string PlayerMood;
     public string FeedbackType;
     public string FeedbackText;
