6748971e6c3c68f9f4e2f9d9d7d5868ee10d7979
Sebastien "Glaas" Decuyper <s.decuyper314@gmail.com>
2021-12-20 19:51:11 +0100

    Added quick toggle on off for the GUI buttons

--- TowerOffense/Assets/Scripts/Christmas/ChristmasMode.cs
+++ TowerOffense/Assets/Scripts/Christmas/ChristmasMode.cs
@@ -6,26 +6,31 @@ using UnityEngine;
 public class ChristmasMode : MonoBehaviour
 {
     private Camera sceneCamera;
-    
+    public bool ShowChristmasMode = false;
+
     private void Start()
     {
         sceneCamera = FindObjectOfType<Camera>(); //todo Camera.main
-        
+
         //christmas layer invisible to main camera
-        sceneCamera.cullingMask = sceneCamera.cullingMask =~ (1 << 12);
+        sceneCamera.cullingMask = sceneCamera.cullingMask = ~(1 << 12);
     }
 
     void OnGUI()
     {
-        if (GUI.Button(new Rect(10, 120, 150, 100), "Activate Xmas Mode"))
+        if (ShowChristmasMode)
+        {
+
+            if (GUI.Button(new Rect(10, 120, 150, 100), "Activate Xmas Mode"))
             {
                 //turn christmas layer on
                 sceneCamera.cullingMask = sceneCamera.cullingMask |= (1 << 12);
             }
-        if (GUI.Button(new Rect(10, 220, 150, 100), "Deactivate Xmas Mode"))
-        {
-            //turn christmas layer off
-            sceneCamera.cullingMask = sceneCamera.cullingMask =~ (1 << 12);
+            if (GUI.Button(new Rect(10, 220, 150, 100), "Deactivate Xmas Mode"))
+            {
+                //turn christmas layer off
+                sceneCamera.cullingMask = sceneCamera.cullingMask = ~(1 << 12);
+            }
         }
     }
 
@@ -33,14 +38,14 @@ public class ChristmasMode : MonoBehaviour
     {
         if (xmasmode)
         {
-            
+
             //turn christmas layer on
             sceneCamera.cullingMask = sceneCamera.cullingMask |= (1 << 12);
         }
         if (!xmasmode)
         {
             //turn christmas layer off
-            sceneCamera.cullingMask = sceneCamera.cullingMask =~ (1 << 12);
+            sceneCamera.cullingMask = sceneCamera.cullingMask = ~(1 << 12);
         }
         else
         {
